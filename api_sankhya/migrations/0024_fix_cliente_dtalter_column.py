b22b220eea1ba040c6ad5a6a9c2f61ed9cc0dd1e
Daniel Nogueira <daniellnogueira@hotmail.com>
2026-05-07 17:01:44 +0100

    Ajuste de migração do sql server para postgres

--- api_sankhya/migrations/0024_fix_cliente_dtalter_column.py
+++ api_sankhya/migrations/0024_fix_cliente_dtalter_column.py
@@ -1,6 +1,39 @@
 from django.db import migrations
 
 
+SQLSERVER_VENDORS = {"microsoft", "mssql", "sql_server"}
+
+
+def _rename_dataalter_to_dtalter(apps, schema_editor):
+    if schema_editor.connection.vendor not in SQLSERVER_VENDORS:
+        return
+
+    schema_editor.execute(
+        """
+IF COL_LENGTH('sankhya_cliente', 'dataalter') IS NOT NULL
+   AND COL_LENGTH('sankhya_cliente', 'dtalter') IS NULL
+BEGIN
+    EXEC sp_rename 'sankhya_cliente.dataalter', 'dtalter', 'COLUMN';
+END
+"""
+    )
+
+
+def _rename_dtalter_to_dataalter(apps, schema_editor):
+    if schema_editor.connection.vendor not in SQLSERVER_VENDORS:
+        return
+
+    schema_editor.execute(
+        """
+IF COL_LENGTH('sankhya_cliente', 'dtalter') IS NOT NULL
+   AND COL_LENGTH('sankhya_cliente', 'dataalter') IS NULL
+BEGIN
+    EXEC sp_rename 'sankhya_cliente.dtalter', 'dataalter', 'COLUMN';
+END
+"""
+    )
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -8,20 +41,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL(
-            sql="""
-IF COL_LENGTH('sankhya_cliente', 'dataalter') IS NOT NULL
-   AND COL_LENGTH('sankhya_cliente', 'dtalter') IS NULL
-BEGIN
-    EXEC sp_rename 'sankhya_cliente.dataalter', 'dtalter', 'COLUMN';
-END
-""",
-            reverse_sql="""
-IF COL_LENGTH('sankhya_cliente', 'dtalter') IS NOT NULL
-   AND COL_LENGTH('sankhya_cliente', 'dataalter') IS NULL
-BEGIN
-    EXEC sp_rename 'sankhya_cliente.dtalter', 'dataalter', 'COLUMN';
-END
-""",
+        migrations.RunPython(
+            _rename_dataalter_to_dtalter,
+            _rename_dtalter_to_dataalter,
         ),
     ]
